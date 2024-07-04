ef60f854664b18825f0c725e94424d3e2634d853
Gospel Darlington <darlingtongospel@gmail.com>
2024-07-04 20:30:05 +0100

    removes unused types

--- utils/types.dt.ts
+++ utils/types.dt.ts
@@ -1,23 +1,7 @@
-export interface TruncateParams {
-  text: string
-  startChars: number
-  endChars: number
-  maxLength: number
-}
-
 export type MintHistoryItem = {
   receiver: any // Consider specifying a more specific type instead of 'any'
   amount: number
   signature: string
   transactionLink: string
   addressLink: string
-}
-
-export interface GlobalState {
-  mintHistory: MintHistoryItem[]
-  balance: number
-}
-
-export interface RootState {
-  globalStates: GlobalState
 }
