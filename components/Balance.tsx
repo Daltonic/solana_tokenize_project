c1ecf83b7d6aa000a1675c0fb84bc45ee4b6c5e5
Gospel Darlington <darlingtongospel@gmail.com>
2024-07-04 19:54:25 +0100

    added a dynamic Identicon

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
+import { useWallet } from '@solana/wallet-adapter-react'
 import Identicon from 'react-identicons'
 
 const Balance = ({ balance }: { balance: number }) => {
+  const { publicKey } = useWallet()
 
   return (
     <div className="bg-white shadow-sm rounded-lg p-3 w-full flex items-center justify-start space-x-4">
       <div className="rounded-full shadow-sm overflow-hidden inline-block">
-        <Identicon string="random-string" size={40} />
+        <Identicon string={publicKey?.toString() || ''} size={40} />
       </div>
       <div>
         <h1 className="text-lg font-semibold text-gray-700">Token Balance</h1>
